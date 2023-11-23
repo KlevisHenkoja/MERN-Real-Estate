1d02517f4ae600641ae204f1f45249e271155408
KlevisHenkoja <klevishn@gmail.com>
2023-11-23 21:30:56 +0100

    Complete User Update Functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEY,
-  authDomain: "real-estate-79a6b.firebaseapp.com",
-  projectId: "real-estate-79a6b",
-  storageBucket: "real-estate-79a6b.appspot.com",
-  messagingSenderId: "724824077540",
-  appId: "1:724824077540:web:bab2c8ef9dc44c99e228f1"
+  apiKey: "AIzaSyCtnR0v3srl086pDSSL9JTcvViZh6mfWyk",
+  authDomain: "mern-realestate-6e108.firebaseapp.com",
+  projectId: "mern-realestate-6e108",
+  storageBucket: "mern-realestate-6e108.appspot.com",
+  messagingSenderId: "132574284469",
+  appId: "1:132574284469:web:4f57bc9eaefb7e11d58cfc"
 };
 
 // Initialize Firebase
